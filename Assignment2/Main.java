406fea43a61aee6fadecef9470d528ce6715867a
Edmund Zhong <edmundzhong2@gmail.com>
2025-03-13 20:09:52 -0400

    updated

--- Assignment2/Main.java
+++ Assignment2/Main.java
@@ -37,3 +37,55 @@ public class Main {
         scanner.close();
     }
 }
+
+/*Pace University
+ *  Spring 2024
+ *  Algorithms and Computing Theory
+ *
+ *  Course: CS 242
+ *  Team members: Edmund Zhong, James Hamilton, Hiruka Gamage
+ *  Collaborators: None
+ *  References: None
+ *
+ *  Assignment: 2
+*  Problem: Quick sort and Radix sort runtime
+ *  Description: We have studied a variety of sorting algorithms assuming that the input
+does not have repetitions for simplicity. Under such assumption, we have
+computed upper bounds on the running time of each algorithm for the worstcase input. But how good/bad are these algorithms if we know that the
+input has a lot of repetitions? The purpose of this assignment is to evaluate
+experimentally the time performance of two sorting algorithms on inputs that
+have many repeated items. Specifically, do the following.
+ *
+ *  Input: 
+ *  Output: 
+ *
+ *  Visible data fields:
+ *  COPY DECLARATION OF VISIBLE DATA FIELDS HERE
+ *
+ *  Visible methods:
+ *  COPY SIGNATURE OF VISIBLE METHODS HERE
+ *
+ *
+ *   Remarks
+ *   -------
+ *
+ *   PUT ALL NON-CODING ANSWERS HERE 
+ * Quicksort Running Time
+ *          1      10     100      1000    10000   100000      -n
+ * 1        0ms     0ms    0ms      0ms     2ms     19ms  
+ * 10       0ms     0ms     0ms     1ms     2ms     17ms
+ * 100      0ms     0ms     0ms     1ms     5ms     25ms
+ * 1000     0ms     0ms     0ms     4ms     16ms    82ms
+ * 10000    0ms     0ms     0ms     5ms     25ms
+ * 100000   0ms     0ms     0ms     4ms     24ms
+ *      
+ * Radix Sort Running Time
+ *          1      10     100      1000    10000   100000      -n
+ * 1        0ms     0ms     0ms     0ms     2ms     11ms
+ * 10       0ms     0ms     0ms     0ms     2ms     11ms
+ * 100      0ms     0ms     0ms     0ms     2ms     12s
+ * 1000     0ms     0ms     0ms     0ms     2ms     11ms
+ * 10000    0ms     0ms     0ms     0ms     2ms
+ * 100000   0ms     0ms     0ms     0ms     2ms
+ * 
+ * */
