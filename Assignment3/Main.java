d72c5a804ae023a858a56fec840de3fc6768bdfd
Edmund Zhong <edmundzhong2@gmail.com>
2025-04-07 21:23:05 -0400

    hi

--- Assignment3/Main.java
+++ Assignment3/Main.java
@@ -5,12 +5,54 @@ import java.util.Random;
 import java.util.Scanner;
 
 // Author: James Hamilton, Edmund Zhong, Hiruka Gamage Ahelpa
-// Date: March 7,2025
+// Date: April 7,2025
 // Purpose/Description: Testing time output of the algorithms found in TextJustification.java
 // Visible Methods and Data: main(String[] args)
+
+/*************************************************************************
+ *
+ *  Pace University
+ *  Spring 2024
+ *  Algorithms and Computing Theory
+ *
+ *  Course: CS 242
+ *  Team members: James Hamilton, Edmund Zhong, Hiruka Gamage Ahelpa
+ *  Collaborators: PUT THE NAME OF ANY COLLABORATORS OUTSIDE YOUR TEAM HERE, IF NONE, PUT NONE
+ *  References: PUT THE LINKS TO YOUR SOURCES HERE
+ *
+ *  Assignment: 3
+ *  Problem: 3
+ *  Description: Dynamic Programming is a powerful technique that explores all possible cases to obtain a solution, yet achieves an exponential improvement in
+running time when compared with a simpler brute-force approach. A wellknown example of Dynamic Programming usage is Text Justification, where
+we are given an array of words and a page width and we have to decide how
+to split the text so that it looks “nice” after justifying against margins. The
+purpose of this homework is to compare experimentally the result obtained
+justifying text with a greedy algorithm (as used by MS Word) against doing
+the same using LATEX rules and Dynamic Programming. An example of text
+justification using Dynamic Programming is attached.
+ *
+ *  Input: 
+ *  Output: 
+ *
+ *  Visible data fields:
+ *  COPY DECLARATION OF VISIBLE DATA FIELDS HERE
+ *
+ *  Visible methods:
+ *  COPY SIGNATURE OF VISIBLE METHODS HERE
+ *
+ *
+ *   Remarks
+ *   -------
+ *
+ *   PUT ALL NON-CODING ANSWERS HERE
+ *
+ *
+ *************************************************************************/
+
 public class Main {
-     public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException {
         Scanner sc = new Scanner(System.in);
+
         System.out.print("Enter number of words: ");
         int n = sc.nextInt();
         System.out.print("Enter page width: ");
@@ -18,16 +60,26 @@ public class Main {
 
         Random random = new Random();
         String[] W = new String[n];
+        
         for (int i = 0; i < n; i++) {
+            // We allow word lengths up to 15
             int len = random.nextInt(15) + 1;
+            // If the generated word is bigger than omega, you either skip it or handle it differently
+            // For now, just ensure it doesn't exceed omega by capping:
+            if (len > omega) {
+                len = omega; 
+            }
             W[i] = "a".repeat(len);
         }
 
         List<Integer> L = TextJustification.split(W, omega);
         TextJustification.justify(W, L, omega, "just.txt");
 
+        // Also write out the "unjustified" version
         FileWriter fw = new FileWriter("unjust.txt");
-        for (String word : W) fw.write(word + " ");
+        for (String word : W) {
+            fw.write(word + " ");
+        }
         fw.close();
 
         System.out.println("Files 'just.txt' and 'unjust.txt' created successfully.");
--- Assignment3/TextJustification.java
+++ Assignment3/TextJustification.java
@@ -4,80 +4,123 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class TextJustification {
-   
+
     // Calculate the badness of a line
     static int badness(String[] W, int i, int j, int omega) {
         int len = 0;
         for (int k = i; k < j; k++) {
             len += W[k].length();
-            if (k > i) len++; // Space between words
+            if (k > i) len++; // One space between words
         }
-        return omega - len >= 0 ? (int) Math.pow(omega - len, 3) : Integer.MAX_VALUE;
+        // If it can't fit, return "infinite" badness:
+        if (omega - len < 0) {
+            return Integer.MAX_VALUE;
+        }
+        // Otherwise, use LATEX-style cubic badness:
+        return (int) Math.pow(omega - len, 3);
     }
 
     // Memoized Minimum Badness function
     static int memoizedMinimumBadness(String[] W, int i, int[] memo, int[] breaks, int omega) {
+        // If we already solved it, return the stored answer
         if (memo[i] >= 0) return memo[i];
+
+        // Base case: if i == W.length, no more words => badness = 0
         if (i == W.length) {
             memo[i] = 0;
-            breaks[i] = W.length;
-        } else {
-            int min = Integer.MAX_VALUE;
-            int index_of_min = i;
-            for (int j = i + 1; j <= W.length; j++) {
-                int temp = badness(W, i, j, omega);
-                if (temp < Integer.MAX_VALUE) {
-                    temp += memoizedMinimumBadness(W, j, memo, breaks, omega);
-                    if (temp < min) {
-                        min = temp;
-                        index_of_min = j;
-                    }
-                }
-            }
-            memo[i] = min;
-            breaks[i] = index_of_min;
+            breaks[i] = W.length; 
+            return 0;
         }
+
+        // Otherwise, try all possible j from i+1..W.length
+        int min = Integer.MAX_VALUE;
+        int indexOfMin = i + 1; // fallback so we won't be stuck on i
+        
+        for (int j = i + 1; j <= W.length; j++) {
+            int currentBadness = badness(W, i, j, omega);
+            if (currentBadness == Integer.MAX_VALUE) {
+                // The words [i..j-1] can't fit on one line; break out early if you like
+                break; 
+            }
+            // If it fits, check total badness (current line + remainder of text)
+            int total = currentBadness + memoizedMinimumBadness(W, j, memo, breaks, omega);
+            if (total < min) {
+                min = total;
+                indexOfMin = j;
+            }
+        }
+
+        memo[i] = min;
+        breaks[i] = indexOfMin;
         return memo[i];
     }
 
-    // Split function
+    // Build the list of line breaks from the DP table
     static List<Integer> split(String[] W, int omega) {
         int[] memo = new int[W.length + 1];
         int[] breaks = new int[W.length + 1];
-        for (int i = 0; i <= W.length; i++) memo[i] = -1;
+        // Initialize memo with -1 
+        for (int i = 0; i < memo.length; i++) {
+            memo[i] = -1;
+        }
 
+        // Compute the DP solution
         memoizedMinimumBadness(W, 0, memo, breaks, omega);
 
+        // Reconstruct the breakpoints
         List<Integer> result = new ArrayList<>();
         int i = 0;
         while (i < W.length) {
             result.add(i);
-            i = breaks[i];
+            // Move i to where we said the next line starts
+            if (breaks[i] == i) {
+                // If breaks[i] never got updated, it means no solution could be found
+                // that fits these words. You might handle that differently if you prefer.
+                // For now, just force a single-word line to avoid infinite loop:
+                i++;
+            } else {
+                i = breaks[i];
+            }
         }
         return result;
     }
 
-    // Justify method
+    // Justify method (writes “fully justified” lines to the file)
     static void justify(String[] W, List<Integer> L, int omega, String filename) throws IOException {
         FileWriter fw = new FileWriter(filename);
+
         for (int idx = 0; idx < L.size(); idx++) {
             int i = L.get(idx);
+            // The next line break is either the next element in L, or the end of W
             int j = (idx == L.size() - 1) ? W.length : L.get(idx + 1);
-            int spaces_needed = omega;
-            for (int k = i; k < j; k++) spaces_needed -= W[k].length();
+
+            // Compute how many total spaces must be distributed
+            int spacesNeeded = omega;
+            for (int k = i; k < j; k++) {
+                spacesNeeded -= W[k].length();
+            }
 
             int gaps = j - i - 1;
             for (int k = i; k < j; k++) {
                 fw.write(W[k]);
+                // Insert spaces if not the last word in the line
                 if (k < j - 1) {
-                    int spaces = spaces_needed / gaps + ((spaces_needed % gaps > 0) ? 1 : 0);
-                    spaces_needed -= spaces;
+                    // Each gap gets at least floor(spacesNeeded/gaps)
+                    // plus possibly 1 more if we have leftover
+                    int spaces = gaps > 0 ? (spacesNeeded / gaps) : 0;
+                    if (gaps > 0 && (spacesNeeded % gaps != 0)) {
+                        spaces += 1;
+                    }
+                    for (int s = 0; s < spaces; s++) {
+                        fw.write(' ');
+                    }
+                    spacesNeeded -= spaces;
                     gaps--;
-                    for (int s = 0; s < spaces; s++) fw.write(" ");
                 }
             }
             fw.write("\n");
         }
+
         fw.close();
     }
 }
